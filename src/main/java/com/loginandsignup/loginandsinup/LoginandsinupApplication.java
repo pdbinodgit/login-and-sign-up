531bbc3c7bde8c55c921be0813ce21055dc0cae8
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2024-07-19 12:02:48 +0545

    feature|setup login and signup project

---
+++ src/main/java/com/loginandsignup/loginandsinup/LoginandsinupApplication.java
@@ -0,0 +1,13 @@
+package com.loginandsignup.loginandsinup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginandsinupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginandsinupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loginandsignup/loginandsinup/LoginandsinupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginandsignup.loginandsinup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginandsinupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
