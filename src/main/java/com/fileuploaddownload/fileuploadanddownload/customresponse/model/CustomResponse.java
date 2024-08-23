b5f61663785c0b7d5e7d958335f2671c58fc8669
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2024-08-23 14:34:21 +0545

    feat| adding custom response class

---
+++ src/main/java/com/fileuploaddownload/fileuploadanddownload/customresponse/model/CustomResponse.java
@@ -0,0 +1,23 @@
+package com.fileuploaddownload.fileuploadanddownload.customresponse.model;
+
+public class CustomResponse<T> {
+    private String status;
+
+    private T data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
